b7236a220a3ea50a9297b991757d6d9b4abe2e9c
Kuruteiru <martin.krutil@email.cz>
2024-05-19 15:41:59 +0200

    added null checks

--- void-pointer/void_pointer.c
+++ void-pointer/void_pointer.c
@@ -4,17 +4,21 @@
 #include <string.h>
 
 void printInt(void *value) {
+    if (!value) return;
     printf("%d", *((int*) value));
 }
 
 void  printString(void *value) {
+    if (!value) return;
     printf("%s", *((char**) value));
 }
 
 bool compareInt(void *value1, void *value2) {
+    if (!value1 || !value2) return false;
     return *((int*) value1) == *((int*) value2);
 }
 
 bool compareString(void *value1, void *value2) {
+    if (!value1 || !value2) return false;
     return strcmp(((char*) value1), ((char*) value2)) == 0;
 }
